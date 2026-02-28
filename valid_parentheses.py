13cd83ba7ddf96a103b375c5c460bef0b0d49d6b
Ytzhak <yr3275744@gmail.com>
2026-03-01 00:25:09 +0200

    Update valid_parentheses.py

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,9 +1,9 @@
 #https://leetcode.com/problems/valid-parentheses/description/
 
 class Solution:
-  """Check whether the parentheses were used correctly. 
-  I used the LIFO principle to make sure that the order of the parentheses was correct, 
-  and that the last parentheses opened were indeed the first ones closed."""
+    """Check whether the parentheses were used correctly. 
+    I used the LIFO principle to make sure that the order of the parentheses was correct, 
+    and that the last parentheses opened were indeed the first ones closed."""
     def isValid(self, s: str) -> bool:
         brackets_value = {"{":1, "(":1, "[":1, "}":-1, ")":-1, "]":-1}
         opposite_pairs = {"{":"}", "(":")", "[":"]"}
@@ -17,6 +17,4 @@ class Solution:
                 last_brackete = brackets_order.pop()
                 if brackete != opposite_pairs[last_brackete]:
                     return False
-        if len(brackets_order) != 0:
-            return False
-        return True
+        return False if len(brackets_order) != 0 else True
